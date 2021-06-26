de47526d999675062653b328b07eab1eafbd6083
rithikJha <rithik.jha.official@gmail.com>
2021-06-26 09:36:15 +0530

    Generic version

--- Sorting Algorithms/Insertion Sort/insertionSortGeneric.cpp
+++ Sorting Algorithms/Insertion Sort/insertionSortGeneric.cpp
@@ -1,38 +1,49 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct student{
+struct student
+{
     string name;
     int roll;
     char grade;
     float marks;
-    friend ostream& operator<<(ostream& out,const student a);
+    friend ostream &operator<<(ostream &out, const student a); //for string representation
 };
-ostream& operator<<(ostream& out, const student a){
-    out<<"{"<<"Name: "<<a.name;
-    out<<" "<<"Roll: "<<a.roll;
-    out<<" "<<"Grade: "<<a.grade;
-    out<<" "<<"Marks: "<<a.marks<<"}\n";
+ostream &operator<<(ostream &out, const student a)
+{
+    out << "{"
+        << "Name: " << a.name;
+    out << " "
+        << "Roll: " << a.roll;
+    out << " "
+        << "Grade: " << a.grade;
+    out << " "
+        << "Marks: " << a.marks << "}\n";
     return out;
 }
 
-template<class dtype>
-void insertionSort(dtype A[], size_t n, function<bool(dtype,dtype)> cmp){
-    for(int i =1;i<int(n);i++){
-        int j=i-1;
+template <class dtype>
+void insertionSort(dtype A[], size_t n, function<bool(dtype, dtype)> cmp)
+{
+    for (int i = 1; i < int(n); i++)
+    {
+        int j = i - 1;
         dtype val = A[i];
-        while(j>=0 && cmp(A[j],val)){
-            A[j+1]=A[j];
+        while (j >= 0 && cmp(A[j], val))
+        {
+            A[j + 1] = A[j];
             j--;
         }
-        A[j+1]=val;
+        A[j + 1] = val;
     }
 }
 
-int main(){
-    student class10[]{{"Rithik",1,'A',96},{"Rick",2,'F',32},{"Mick",3,'B',78},{"Wick",4,'C',66}};
-    size_t n = sizeof(class10)/sizeof(class10[0]);
+int main()
+{
+    student class10[]{{"Rithik", 1, 'A', 96}, {"Rick", 2, 'F', 32}, {"Mick", 3, 'B', 78}, {"Wick", 4, 'C', 66}};
+    size_t n = sizeof(class10) / sizeof(class10[0]);
     // Sort according to marks in ascending order
-    insertionSort<student>(class10,n,[](student a, student b){return a.marks>b.marks;});
-    for(int i=0;i<n;i++)
-        cout<<class10[i];
+    insertionSort<student>(class10, n, [](student a, student b)
+                           { return a.marks > b.marks; });
+    for (int i = 0; i < n; i++)
+        cout << class10[i];
 }
